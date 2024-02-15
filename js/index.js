f80396796052f013770e0f88b34f1851c6a22b23
sabbir ahmed <sabbirahmed.info33@gmail.com>
2024-02-15 21:56:15 +0600

    my last commit

--- js/index.js
+++ js/index.js
@@ -1,19 +1,27 @@
 function keyuprase(event) {
   const keyEvent = event.key;
 
+  if (keyEvent === "Escape") {
+    hideItem("play-graund");
+    removeIemandshow("Your-score");
+  }
+
   const mygrawond = document.getElementById("graound");
   const currentAlpabet = mygrawond.innerText;
   const lettar = currentAlpabet.toLowerCase();
 
-  console.log(lettar, keyEvent);
-
   if (keyEvent === lettar) {
     reomebackground(lettar);
     continewGame();
-    CountScore("current-scor")
-} else {
-      Incriselife('life')
-      
+    CountScore("current-scor");
+    const result = document.getElementById("result");
+    result.innerText = CountScore("result");
+  } else {
+    const stop = Incriselife("life");
+    if (stop === 0) {
+      hideItem("play-graund");
+      removeIemandshow("Your-score");
+    }
   }
 }
 
@@ -28,6 +36,17 @@ function continewGame() {
   const result = alfabate();
   const graound = document.getElementById("graound");
   graound.innerText = result;
-
   setBackgroundColor(result);
+}
+
+document.getElementById("play-agean").addEventListener("click", playagen);
+
+// play agean
+function playagen() {
+  hideItem("Your-score");
+  removeIemandshow("play-graund");
+  newdicremant("life", 10);
+  newincrimant("current-scor", 0);
+  //   continewGame();
+  setTextElemant();
 }
--- js/utilaty.js
+++ js/utilaty.js
@@ -1,16 +1,16 @@
-// this is the hide item 
+// this is the hide item
 function hideItem(elemantid) {
-  const elemant = document.getElementById("home");
+  const elemant = document.getElementById(elemantid);
   elemant.classList.add("hidden");
 }
 
-// sho elemant 
+// sho elemant
 function removeIemandshow(showElemant) {
-  const show = document.getElementById("play-graund");
+  const show = document.getElementById(showElemant);
   show.classList.remove("hidden");
 }
 
-// this is the alphabet elemant 
+// this is the alphabet elemant
 function alfabate() {
   const alpabateString = "abcdefghijklmnopqrstuvwxyz";
   const alfabates = alpabateString.split("");
@@ -22,34 +22,51 @@ function alfabate() {
   return alpabets;
 }
 
-
-// this is the setBackgroundColor 
+// this is the setBackgroundColor
 function setBackgroundColor(elmantId) {
   const elemant = document.getElementById(elmantId);
-  elemant.classList.add('bg-[#FFA500]');
+  elemant.classList.add("bg-[#FFA500]");
 }
 
-// remove backgroundColor 
-function reomebackground (elem){
-    const newElemant = document.getElementById(elem);
-    newElemant.classList.remove('bg-[#FFA500]')
+// remove backgroundColor
+function reomebackground(elem) {
+  const newElemant = document.getElementById(elem);
+  newElemant.classList.remove("bg-[#FFA500]");
 }
 
-// scor Count 
-function CountScore(elem){
-const scorElemant = document.getElementById(elem);
-let  scorText = scorElemant.innerText
-let  scor = parseInt(scorText)
-scor++
-const finalScore = scorElemant.innerText = scor
- return finalScore
+// scor Count
+function CountScore(elem) {
+  const scorElemant = document.getElementById(elem);
+  let scorText = scorElemant.innerText;
+  let scor = parseInt(scorText);
+  scor++;
+  const finalScore = (scorElemant.innerText = scor);
+  return finalScore;
 }
 
-function Incriselife(elem){
+// dicrimant count
+function Incriselife(elem) {
   const mylife = document.getElementById(elem);
   const lifeText = mylife.innerText;
   let life = parseInt(lifeText);
-  life--
-  const finallife = mylife.innerText = life;
+  life--;
+  const finallife = (mylife.innerText = life);
   return finallife;
+}
+
+// play ageanDicremant
+function newdicremant(elem, value) {
+  const elemant = document.getElementById(elem);
+  elemant.innerText = value;
+}
+//   play ageanIncremant
+function newincrimant(elem, value) {
+  const elemant = document.getElementById(elem);
+  elemant.innerText = value;
+}
+
+function setTextElemant() {
+  const newgrawond = document.getElementById("graound");
+  const playagaon = newgrawond.innerText;
+  return playagaon
 }
